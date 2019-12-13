48f3f4bf1e2e6c30f7f1fa31049d5a14a07c5f5f
No758 <272945808@qq.com>
2019-12-13 13:42:36 +0800

    Started with C++

---
+++ hello.cpp
@@ -0,0 +1,14 @@
+#include<iostream> 
+using namespace std;
+
+int main()
+{
+    cout<<"Hello world!"<<endl;
+    cout<<"Enter two number:"<<endl;
+    int v1,v2;
+    cin>>v1>>v2;
+    cout<<"The sum of " <<v1<<" and "<<v2<<" is "
+    <<v1+v2<<endl;
+  
+    return 0;
+}
